f0c98318705a18f8ec8ef6f6f247a37033b97627
chris-bingham <chris.bingham@aerian.com>
2019-05-08 16:35:39 +0100

    line rotating and egg firing in corrrect dir

--- src/components/bubble-pop-game.js
+++ src/components/bubble-pop-game.js
@@ -9,7 +9,6 @@ export class BubblePopGame extends Screen {
   create() {
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.theme = this.context.config.theme[this.game.state.current];
-    console.log(this);
 
     this.addAssets();
 
@@ -19,6 +18,15 @@ export class BubblePopGame extends Screen {
   }
 
   render() {}
+
+  update() {
+    this.aimLine.rotation =
+      this.game.physics.arcade.angleToPointer(
+        this.aimLine.children[0],
+        this.input.activePointer,
+        true
+      ) + 1.57;
+  }
 
   addAssets() {
     this.addBackground();
@@ -34,17 +42,40 @@ export class BubblePopGame extends Screen {
   }
 
   addShooter() {
-    const bottomY = this.game.height / 2;
-    const shooterImage = this.game.add.sprite(
+    this.shooter = this.game.add.sprite(
       this.theme.shooter.position.x,
       this.theme.shooter.position.y,
       "game.shooter"
     );
-    this.scene.addToBackground(shooterImage);
-    shooterImage.anchor.setTo(
+    this.scene.addToBackground(this.shooter);
+    this.shooter.physicsBodyType = Phaser.Physics.ARCADE;
+    this.shooter.anchor.setTo(
       this.theme.shooter.anchor.x,
       this.theme.shooter.anchor.y
-    ); // ?move into config??
+    );
+    this.shooter.angle = 0;
+
+    console.log(this.shooter);
+
+    this.aimLine = this.game.add.group(
+      this.theme.shooter.position.x,
+      this.theme.shooter.position.y
+    );
+    this.aimLine.physicsBodyType = Phaser.Physics.ARCADE;
+    for (let i = 0; i < 20; i++) {
+      //? swap for image?
+      const graphics = this.game.add.graphics(0, 0);
+      graphics.lineStyle(0);
+      graphics.beginFill(0xffff0b, 0.5);
+      graphics.drawCircle(0, -(i * 20), 10);
+      graphics.endFill();
+      this.aimLine.add(graphics);
+    }
+    console.log(this.aimLine.world);
+
+    this.shooter.addChild(this.aimLine);
+    // this.aimLine.rotation = 1.57;
+    // this.aimLine.anchor.setTo(0.5, 1.0);
   }
 
   createAmmo() {
@@ -56,7 +87,7 @@ export class BubblePopGame extends Screen {
 
     // TO DO: read from array here and create the correct ammo types in correct order
     const bulletImage = "game." + "game_button_2_0";
-    this.ammo.createMultiple(10, bulletImage);
+    this.ammo.createMultiple(100, bulletImage);
     this.ammo.setAll("scale.x", 0.3);
     this.ammo.setAll("scale.y", 0.3);
   }
@@ -66,12 +97,12 @@ export class BubblePopGame extends Screen {
       const bullet = this.ammo.getFirstExists(false);
 
       if (bullet) {
-        bullet.reset(0, 0);
+        bullet.reset(this.shooter.x, this.shooter.y);
         this.scene.addToBackground(bullet);
         bullet.lifespan = 2000;
         this.game.physics.arcade.velocityFromRotation(
-          0,
-          500,
+          this.aimLine.rotation - 1.57,
+          1000,
           bullet.body.velocity
         );
         this.allowFireTime = this.game.time.now + 50;
