8f30c596622a594f7d014eba7a683420be3f3889
chris-bingham <chris.bingham@aerian.com>
2019-05-09 14:03:21 +0100

    targets and collision

--- src/components/bubble-pop-game.js
+++ src/components/bubble-pop-game.js
@@ -26,13 +26,16 @@ export class BubblePopGame extends Screen {
         this.input.activePointer,
         true
       ) + 1.57;
+    
+    this.physics.arcade.collide(this.targets, this.ammo, this.collision, null, this);
   }
 
   addAssets() {
     this.addBackground();
     this.addShooter();
+    this.addTargets();
     this.createAmmo();
-    //addTargets
+
     //addForegroundCharacter
   }
 
@@ -41,7 +44,24 @@ export class BubblePopGame extends Screen {
     return this.scene.addToBackground(backgroundImage);
   };
 
+  addTargets = () => {
+    this.targets = this.game.add.group();
+    this.game.physics.arcade.enable(this.targets);
+    this.targets.enableBody = true;
+
+    const target1 = this.game.add.sprite(-50, -300, "game." + "game_button_1_0");
+    const target2 = this.game.add.sprite(50, -300, "game." + "game_button_2_0");
+    this.targets.add(target1);
+    this.targets.add(target2);
+    this.targets.setAll("body.immovable", true);
+    this.targets.setAll("scale.x", 0.1);
+    this.targets.setAll("scale.y", 0.1); 
+    this.scene.addToBackground(this.targets);
+  }
+
   addShooter = () => {
+    // empty sprite in location that theme depicts we should shoot from
+    // contains the aimLine group of elements as a child
     this.shootFrom = this.game.add.sprite(
       this.theme.shootFrom.position.x,
       this.theme.shootFrom.position.y
@@ -84,24 +104,41 @@ export class BubblePopGame extends Screen {
     this.ammo = this.game.add.group();
     this.ammo.enableBody = true;
     this.ammo.physicsBodyType = Phaser.Physics.ARCADE;
-
+    // this.ammo.body.onCollide.add(collision, this);
     // TO DO: read from array here and create the correct ammo types in correct order
     const bulletImage = "game." + "game_button_2_0";
     this.ammo.createMultiple(100, bulletImage);
-    this.ammo.setAll("scale.x", 0.3);
-    this.ammo.setAll("scale.y", 0.3);
+    this.ammo.setAll("scale.x", 0.1);
+    this.ammo.setAll("scale.y", 0.1);
+    this.ammo.callAll(
+      "events.onOutOfBounds.add",
+      "events.onOutOfBounds",
+      this.killBubble
+    );
+    this.ammo.setAll("checkWorldBounds", true);
 
     this.addNextAmmoToScene();
   };
 
+  killBubble = bubble => {
+    bubble.kill();
+  };
+
+  collision = (target, bullet) => {
+      bullet.body.velocity.y = 0;
+      bullet.body.velocity.x = 0;
+      this.ammo.remove(bullet);
+      this.targets.add(bullet);
+      bullet.body.immovable = true;
+  };
+
   fireBubble = () => {
     if (this.game.time.now > this.allowFireTime) {
-      if (this.currentBullet) {
-        this.currentBullet.lifespan = 2000;
+      if (this.currentBubble) {
         this.game.physics.arcade.velocityFromRotation(
           this.aimLine.rotation - 1.57,
           1000,
-          this.currentBullet.body.velocity
+          this.currentBubble.body.velocity
         );
         this.allowFireTime = this.game.time.now + 200;
 
@@ -111,12 +148,13 @@ export class BubblePopGame extends Screen {
   };
 
   addNextAmmoToScene = () => {
-    this.currentBullet = this.ammo.getChildAt(this.nextAmmoIndex);
-    if (!this.currentBullet) {
+    this.currentBubble = this.ammo.getChildAt(this.nextAmmoIndex);
+    if (!this.currentBubble) {
       this.nextAmmoIndex = 0;
       this.addNextAmmoToScene();
     }
-    this.currentBullet.reset(this.shootFrom.x, this.shootFrom.y);
-    this.scene.addToBackground(this.currentBullet);
+    this.currentBubble.reset(this.shootFrom.x, this.shootFrom.y);
+    this.game.physics.arcade.enable(this.currentBubble);
+    this.scene.addToBackground(this.currentBubble);
   };
 }
