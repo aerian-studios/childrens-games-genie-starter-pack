ad0c1cb77e99532998612bb2ead75f5e0b481e54
chris-bingham <chris.bingham@aerian.com>
2019-05-20 16:49:53 +0100

    switched to p2 physics

--- src/components/bubble-pop-game.js
+++ src/components/bubble-pop-game.js
@@ -7,7 +7,18 @@ export class BubblePopGame extends Screen {
   }
 
   create() {
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.startSystem(Phaser.Physics.P2JS);
+    this.game.physics.startSystem(Phaser.Physics.Arcade);
+
+    //  Turn on impact events for the world, without this we get no collision callbacks
+    this.game.physics.p2.setImpactEvents(true);
+
+    this.game.physics.p2.restitution = 0.8;
+
+    //  Create our collision groups. One for the player, one for the pandas
+    this.ammoCollisionGroup = this.game.physics.p2.createCollisionGroup();
+    this.targetCollisionGroup = this.game.physics.p2.createCollisionGroup();
+
     this.theme = this.context.config.theme[this.game.state.current];
     this.addComponents();
 
@@ -27,13 +38,13 @@ export class BubblePopGame extends Screen {
       ) + 1.57;
 
     this.aimLine.rotation = angleToPointer;
-    this.physics.arcade.collide(
-      this.targets,
-      this.currentBubble,
-      this.collision,
-      null,
-      this
-    );
+    // this.physics.arcade.collide(
+    //   this.targets,
+    //   this.currentBubble,
+    //   this.collision,
+    //   null,
+    //   this
+    // );
     this.physics.arcade.collide(this.walls, this.ammo);
   }
 
@@ -74,18 +85,23 @@ export class BubblePopGame extends Screen {
 
   addTargets = () => {
     this.targets = this.game.add.group();
-    this.game.physics.arcade.enable(this.targets);
+    // this.game.physics.arcade.enable(this.targets);
     this.targets.enableBody = true;
+    this.targets.physicsBodyType = Phaser.Physics.P2JS;
 
-    const target1 = this.game.add.sprite(
-      -50,
-      -300,
-      "game." + "game_button_1_0"
-    );
-    const target2 = this.game.add.sprite(50, -300, "game." + "game_button_2_0");
-    this.targets.add(target1);
-    this.targets.add(target2);
-    this.targets.setAll("body.immovable", true);
+    const target1 = this.targets.create(0, -300, "game." + "game_button_1_0");
+    const target2 = this.targets.create(50, -300, "game." + "game_button_2_0");
+    // this.targets.add(target1);
+    // this.targets.add(target2);
+
+    target1.body.setCircle(10);
+    target2.body.setCircle(10);
+    target1.body.setCollisionGroup(this.targetCollisionGroup);
+    target2.body.setCollisionGroup(this.targetCollisionGroup);
+    target1.body.collides(this.ammoCollisionGroup);
+    target2.body.collides(this.ammoCollisionGroup);
+
+    this.targets.setAll("body.kinematic", true);
     this.targets.setAll("scale.x", 0.1);
     this.targets.setAll("scale.y", 0.1);
     this.scene.addToBackground(this.targets);
@@ -125,9 +141,9 @@ export class BubblePopGame extends Screen {
   createAmmo = () => {
     this.allowFireTime = 0; // used to prevent multiple shots within 50ms of one another
 
-    this.ammo = this.game.add.physicsGroup();
-    // this.ammo.enableBody = true;
-    // this.ammo.physicsBodyType = Phaser.Physics.ARCADE;
+    this.ammo = this.game.add.group();
+    this.ammo.enableBody = true;
+    this.ammo.physicsBodyType = Phaser.Physics.P2JS;
     // this.game.physics.arcade.enable(this.ammo);
 
     this.addAmmoToGroup();
@@ -149,7 +165,7 @@ export class BubblePopGame extends Screen {
     //   this.killBubble
     // );
 
-    this.ammo.callAll("body.bounce.set", "body.bounce", 1);
+    // this.ammo.callAll("body.bounce.set", "body.bounce", 1);
     // this.ammo.setAll("checkWorldBounds", true);
 
     this.addNextAmmoToScene();
@@ -157,13 +173,24 @@ export class BubblePopGame extends Screen {
 
   addAmmoToGroup = () => {
     let i = 0;
+    let j = 0;
     while (this.ammo.length <= (this.theme.amountOfAmmo || 100)) {
-      const sprite = this.createBullet(this.theme.ammo[i]);
-      this.ammo.add(sprite);
+      const sprite = this.ammo.create(
+        j * 100,
+        j * 100,
+        "game.bubble_" + this.theme.ammo[i]
+      );
+      sprite.body.setCircle(5);
+      sprite.body.setCollisionGroup(this.ammoCollisionGroup);
+      sprite.body.collides([this.targetCollisionGroup], this.collision);
+      // this.game.physics.enable(sprite);
+      // this.ammo.add(sprite);
       i++;
       if (i >= this.theme.ammo.length) {
         i = 0;
       }
+
+      j++;
     }
   };
 
@@ -188,13 +215,25 @@ export class BubblePopGame extends Screen {
   };
 
   collision = (bullet, target) => {
-    this.ammo.remove(bullet);
-    this.targets.add(bullet);
-    console.log("Ammo", this.ammo.length, "targets", this.targets.length);
-    bullet.body.velocity.y = 0;
-    bullet.body.velocity.x = 0;
-    bullet.enableBody = false;
-    bullet.body.immovable = true;
+    console.log("SMASHHH");
+
+    bullet.setZeroVelocity();
+    bullet.kinematic = true;
+    bullet.setZeroRotation();
+    target.setZeroVelocity();
+
+    target.setZeroRotation();
+    bullet.removeCollisionGroup(this.ammoCollisionGroup);
+    bullet.setCollisionGroup(this.targetCollisionGroup);
+    bullet.collides(this.ammoCollisionGroup, this.collision);
+    console.log(bullet);
+    // this.targets.add(bullet);
+
+    // console.log("Ammo", this.ammo.length, "targets", this.targets.length);
+    // bullet.body.velocity.y = 0;
+    // bullet.body.velocity.x = 0;
+    // // bullet.enableBody = false;
+    // bullet.body.immovable = true;
     // this.killBubble(bullet);
     // this.checkIfNeedMoreAmmo(5);
   };
@@ -203,11 +242,13 @@ export class BubblePopGame extends Screen {
     if (this.game.time.now > this.allowFireTime) {
       if (this.nextBubble) {
         this.currentBubble = this.nextBubble;
-        this.game.physics.arcade.velocityFromRotation(
-          this.aimLine.rotation - 1.57,
-          1000,
-          this.currentBubble.body.velocity
-        );
+        // this.game.physics.arcade.velocityFromRotation(
+        //   this.aimLine.rotation - 1.57,
+        //   1000,
+        //   this.currentBubble.body.velocity
+        // );
+        this.nextBubble.body.rotation = this.aimLine.rotation;
+        this.nextBubble.body.moveForward(400);
         this.allowFireTime = this.game.time.now + 200;
 
         setTimeout(this.addNextAmmoToScene, 150); // swap for phaser timer
@@ -219,11 +260,12 @@ export class BubblePopGame extends Screen {
     if (!this.ammo.length) {
       return;
     }
-    this.nextBubble = this.ammo.getFirstExists();
-    if (!this.nextBubble) {
-      this.addNextAmmoToScene();
+    this.nextBubble = this.ammo.getChildAt(0);
+    if (this.nextBubble) {
+      this.nextBubble.reset(this.shootFrom.x, this.shootFrom.y);
+      this.scene.addToBackground(this.nextBubble);
+    } else {
+      console.log("!!! NO AMMO REMAINING !!!");
     }
-    this.nextBubble.reset(this.shootFrom.x, this.shootFrom.y);
-    this.scene.addToBackground(this.nextBubble);
   };
 }
