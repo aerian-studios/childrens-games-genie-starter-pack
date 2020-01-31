2ee454fb06646812df5a62c7f49a53d06e91d55e
chris-bingham <chris.bingham@aerian.com>
2020-01-31 13:33:57 +0000

    Ready for session start

--- src/main.js
+++ src/main.js
@@ -8,83 +8,74 @@ import { settingsChannel } from "../node_modules/genie/src/core/settings.js";
 import * as signal from "../node_modules/genie/src/core/signal-bus.js";
 
 const settingsConfig = {
-    pages: [
+  pages: [
+    {
+      title: "Custom Settings",
+      settings: [
         {
-            title: "Custom Settings",
-            settings: [
-                {
-                    key: "custom1",
-                    type: "toggle",
-                    title: "Custom setting",
-                    description: "Description of custom setting",
-                },
-            ],
-        },
-    ],
+          key: "custom1",
+          type: "toggle",
+          title: "Custom setting",
+          description: "Description of custom setting"
+        }
+      ]
+    }
+  ]
 };
 
 signal.bus.subscribe({
-    channel: settingsChannel,
-    name: "custom1",
-    callback: value => {
-        console.log("Custom 1 setting changed to " + value);
-    },
+  channel: settingsChannel,
+  name: "custom1",
+  callback: value => {
+    console.log("Custom 1 setting changed to " + value);
+  }
 });
 
 const navigationConfig = goToScreen => {
-    const goToHome = data => goToScreen("home", data);
-    const goToCharacterSelect = data => goToScreen("character-select", data);
-    const goTolevelSelect = data => goToScreen("level-select", data);
-    const goToGame = data => goToScreen("game", data);
-    const goToResults = data => goToScreen("results", data);
+  const goToHome = data => goToScreen("home", data);
+  const goToCharacterSelect = data => goToScreen("character-select", data);
+  const goToGame = data => goToScreen("game", data);
+  const goToResults = data => goToScreen("results", data);
 
-    return {
-        loadscreen: {
-            state: Loadscreen,
-            routes: {
-                next: goToHome,
-            },
-        },
-        home: {
-            state: Home,
-            routes: {
-                next: goToCharacterSelect
-            },
-        },
-        "character-select": {
-            state: Select,
-            routes: {
-                next: goTolevelSelect,
-                home: goToHome,
-                restart: goToHome,
-            },
-        },
-        "level-select": {
-            state: Select,
-            routes: {
-                next: goToGame,
-                home: goToHome,
-                restart: goToHome,
-            },
-        },
-        game: {
-            state: ClickProgressionGame,
-            routes: {
-                next: goToResults,
-                home: goToHome,
-                restart: goToGame,
-            },
-        },
-        results: {
-            state: Results,
-            routes: {
-                next: goToHome,
-                game: goToGame,
-                restart: goToGame,
-                home: goToHome,
-            },
-        }
-    };
+  return {
+    loadscreen: {
+      state: Loadscreen,
+      routes: {
+        next: goToHome
+      }
+    },
+    home: {
+      state: Home,
+      routes: {
+        next: goToCharacterSelect
+      }
+    },
+    "character-select": {
+      state: Select,
+      routes: {
+        next: goToGame,
+        home: goToHome,
+        restart: goToHome
+      }
+    },
+    game: {
+      state: ClickProgressionGame,
+      routes: {
+        next: goToResults,
+        home: goToHome,
+        restart: goToGame
+      }
+    },
+    results: {
+      state: Results,
+      routes: {
+        next: goToHome,
+        game: goToGame,
+        restart: goToGame,
+        home: goToHome
+      }
+    }
+  };
 };
 
 startup(settingsConfig, navigationConfig);
