d459d84413a8ecbde47d20d744ea801f8025e575
Śūnya Dickman <sunya.dickman@aerian.com>
2020-01-31 14:57:35 +0000

    setup changes

--- src/components/click-progression-game.js
+++ src/components/click-progression-game.js
@@ -5,5 +5,26 @@ export class ClickProgressionGame extends Screen {
     super();
   }
 
-  create() {}
+  create() {
+    this.theme = this.context.config.theme.game;
+    console.log(this.context.config.theme.game);
+    const text = this.game.add.text(
+      this.theme.text.position.x,
+      this.theme.text.position.y,
+      this.theme.text.content,
+      this.theme.text.style
+    );
+
+    this.scene.addToBackground(text);
+
+    const gameButton = this.game.add.button(
+      100,
+      100,
+      "game.game_button",
+      () => console.log("clicked", this),
+      this
+    );
+
+    this.scene.addToBackground(gameButton);
+  }
 }
