8575f7979452ae6111c7d33ee044618ed5159f1b
Śūnya Dickman <sunya.dickman@aerian.com>
2020-02-07 17:05:03 +0000

    feat: Added sprite atlas & screen naviagation & achievment

--- src/components/click-progression-game.js
+++ src/components/click-progression-game.js
@@ -1,4 +1,6 @@
 import { Screen } from "../../node_modules/genie/src/core/screen.js";
+import { accessibilify } from "../../node_modules/genie/src/core/accessibility/accessibilify.js";
+import { gmi } from "../../node_modules/genie/src/core/gmi/gmi.js";
 
 export class ClickProgressionGame extends Screen {
   constructor() {
@@ -6,7 +8,15 @@ export class ClickProgressionGame extends Screen {
   }
 
   create() {
+    const settings = gmi.getAllSettings();
+    console.log({ gmi, settings });
+
+    gmi.setGameData("foo", "bar");
+
+    this.scene.addLayout(["exit", "pause"]);
     this.theme = this.context.config.theme.game;
+    const choiceIndex = this.transientData["character-select"].index + 1;
+
     console.log(this.context.config.theme.game);
     const text = this.game.add.text(
       this.theme.text.position.x,
@@ -20,16 +30,39 @@ export class ClickProgressionGame extends Screen {
 
     const buttonClicked = () => {
       this.clickCount = this.clickCount < 9 ? this.clickCount + 1 : 9;
-      gameButton.loadTexture(`game.game_button_1_${this.clickCount}`);
+      gmi.achievements.set({
+        key: "two_clicks",
+        progress: this.clickCount
+      });
+      if (this.clickCount >= 9) {
+        console.log(this.transientData);
+        this.transientData.results = "YAYAYAYAYA!!!!";
+
+        this.navigation.next();
+        return;
+      }
+
+      const egg = `egg_${choiceIndex}_${this.clickCount}`;
+      console.log({ egg });
+
+      gameButton.setFrames(egg, egg);
     };
 
     const gameButton = this.game.add.button(
-      100,
-      100,
-      "game.game_button_1_0",
+      this.theme.gameButton.position.x,
+      this.theme.gameButton.position.y,
+      `game.texture-atlas`,
       buttonClicked,
-      this
+      this,
+      `egg_${choiceIndex}_0`
     );
+
+    const config = {
+      id: "game-button",
+      ariaLabel: "Click to progress"
+    };
+
+    accessibilify(gameButton, config);
 
     this.scene.addToBackground(gameButton);
   }
